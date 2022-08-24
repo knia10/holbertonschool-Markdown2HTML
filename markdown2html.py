b3a048f69ccf03b3473494d4f24fe879af793963
knia10 <kelly.stephania@hotmail.com>
2022-08-24 17:44:54 -0500

    documented file

--- markdown2html.py
+++ markdown2html.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-'''
+"""
 Write a script markdown2html.py that takes an argument 2 strings:
 
 First argument is the name of the Markdown file
 Second argument is the output file name
-'''
+"""
 
 from sys import argv
 from os import path
 import markdown
 from markdown import markdown
 if __name__ == "__main__":
-    '''function for markdown'''
+    """function for markdown"""
     def uso():
         if len(argv) < 3:
             print("Usage: ./markdown2html.py README.md README.html")
